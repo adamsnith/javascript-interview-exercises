c7d76f528a15ddca0ee78eed765f0753f43537df
Adam <adam@typsnith.se>
2019-04-13 12:25:17 +0200

    Exercise1

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 (() => {
+    const companies = [
+        { id: 1, name: "Amazon", location: "Seattle" },
+        { id: 2, name: "Apple", location: "Cupertino" },
+        { id: 3, name: "Facebook", location: "Menlo Park" },
+        { id: 4, name: "Google", location: "Mountain View" },
+        { id: 5, name: "Leeroy", location: "Sundsvall" },
+        { id: 6, name: "Tesla", location: "Palo Alto" }
+    ];
 
-  const companies = [
-    {id: 1, name: 'Amazon', location: 'Seattle'},
-    {id: 2, name: 'Apple', location: 'Cupertino'},
-    {id: 3, name: 'Facebook', location: 'Menlo Park'},
-    {id: 4, name: 'Google', location: 'Mountain View'},
-    {id: 5, name: 'Leeroy', location: 'Sundsvall'},
-    {id: 6, name: 'Tesla', location: 'Palo Alto'}
-  ]
+    // Get the list
+    const list = document.getElementById("list");
 
-})()
+    // Wrap the name and location of each company
+    const listItems = companies
+        .map(company => `<li>${company.name} - ${company.location}</li>`)
+        .join("");
+
+    // Attach to list
+    list.innerHTML = listItems;
+})();
